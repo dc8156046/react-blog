e1b9f132a455c28341d20e2f2250b081fcacd76d
Dan Chen <dan.chen01@edu.sait.ca>
2024-11-15 11:40:29 -0700

    modify about page css

--- app/about/page.js
+++ app/about/page.js
@@ -10,36 +10,44 @@ export default function AboutPage() {
           <h1 className="text-4xl font-bold text-gray-800 mb-3 border-b border-gray-300 pb-2">
             About Page
           </h1>
-          <div className="flex items-center mb-4">
-            <p>
+          <div className="flex flex-col items-start mb-4">
+            <p className="text-gray-800 mb-4">
               This is a simple blog application built with React, Tailwind CSS,
               and FastAPI.
             </p>
-            <p>
+            <p className="text-gray-800 mb-4">
               The application has three main features: Home, Posts, and About.
               The Home page displays a list of posts, the Posts page (needs
               login) allows you to add, edit, and delete posts, and the About
               page provides information about the application.
             </p>
-            <p>
+            <p className="text-gray-800 mb-4">
               The application uses React Router to manage navigation, and it
               uses React Hooks to manage state and side effects. Tailwind CSS is
               used for styling, and the JSONPlaceholder API is used to fetch and
               update data.
             </p>
-            <p>
+            <p className="text-gray-800 mb-4">
               The application demonstrates how to build a simple blog using
               React, Tailwind CSS, and FastAPI. It covers common features like
               fetching data from an API, handling user input, and managing state
               in a React application.
             </p>
             <h2 className="text-xl font-semibold text-gray-700 mb-2">
-              About developer
+              About Developer
             </h2>
-            <p>
-              This application was developed by Dan Chen. You can find the
-              source code on GitHub at https://github.com/dc8156046/react-blog.
-              Feel free to reach out if you have any questions or feedback.
+            <p className="text-gray-800">
+              This application was developed by{" "}
+              <span className="font-medium text-blue-600">Dan Chen</span>. You
+              can find the source code on GitHub at
+              <a
+                href="https://github.com/dc8156046/react-blog"
+                target="_blank"
+                className="text-blue-500 hover:underline ml-1"
+              >
+                https://github.com/dc8156046/react-blog
+              </a>
+              . Feel free to reach out if you have any questions or feedback.
             </p>
           </div>
         </div>
